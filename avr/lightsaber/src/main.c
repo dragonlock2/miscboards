51f53612651c8510efec7697aef9c0be8e580281
Matthew Tran <matthewlamtran@berkeley.edu>
2023-01-20 00:42:47 -0800

    flashed to attiny817

--- avr/lightsaber/src/main.c
+++ avr/lightsaber/src/main.c
@@ -1,13 +1,18 @@
-#define F_CPU 3300000L
+#define F_CPU 20000000L
 
 #include <avr/io.h>
 #include <util/delay.h>
+// #include <stdio.h>
 
 int main(void) {
-    PORTC.DIRSET = PIN0_bm;
+    // set to 20MHz
+    CCP = CCP_IOREG_gc;
+    CLKCTRL.MCLKCTRLB = 0x00;
 
+    PORTC.DIRSET = PIN0_bm;
     while (1) {
-        // TODO call memcpy and pow and printf
+        // puts("hello world!\r\n");
+
         PORTC.OUTTGL = PIN0_bm;
         _delay_ms(500);
     }
