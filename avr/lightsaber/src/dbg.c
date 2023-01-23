9bd06c8430546b067ec6913a943fecf4be1b99ed
Matthew Tran <matthewlamtran@berkeley.edu>
2023-01-22 22:48:17 -0800

    got printf working

--- avr/lightsaber/src/dbg.c
+++ avr/lightsaber/src/dbg.c
@@ -3,20 +3,26 @@
 #include <util/delay.h>
 #include "dbg.h"
 
+#define BAUD_RATE (115200L)
+
 /* private helpers */
-int uart_putc(char var, FILE *stream) {
-    // TODO use UART
-    PORTC.OUTTGL = PIN0_bm;
-    _delay_ms(20);
+static int uart_putc(char c, FILE *f) {
+    USART0.TXDATAL = c;
+    while (!(USART0.STATUS & USART_TXCIF_bm));
+    USART0.STATUS |= USART_TXCIF_bm;
     return 0;
 }
 
 /* public functions */
-FILE mystdout = FDEV_SETUP_STREAM(uart_putc, NULL, _FDEV_SETUP_WRITE);
-
 void dbg_init() {
-    // TODO setup UART
-    PORTC.DIRSET = PIN0_bm;
+    USART0.BAUD  = 4L * F_CPU / BAUD_RATE; // 64*fclk_per/16/fbaud
+    USART0.CTRLC = USART_CMODE_ASYNCHRONOUS_gc |
+                   USART_PMODE_DISABLED_gc |
+                   USART_SBMODE_1BIT_gc |
+                   USART_CHSIZE_8BIT_gc; // 8N1 UART
+    PORTB.DIRSET = PIN2_bm; // TX (PB2) as output
+    USART0.CTRLB = USART_TXEN_bm | USART_RXMODE_NORMAL_gc; // enable TX
 
-    stdout = &mystdout;
+    static FILE uart_stdout = FDEV_SETUP_STREAM(uart_putc, NULL, _FDEV_SETUP_WRITE);
+    stdout = &uart_stdout;
 }
--- avr/lightsaber/src/main.c
+++ avr/lightsaber/src/main.c
@@ -3,17 +3,23 @@
 #include <util/delay.h>
 #include "dbg.h"
 
-int main(void) {
-    // set to 20MHz
+void clk_init() {
     CCP = CCP_IOREG_gc;
-    CLKCTRL.MCLKCTRLB = 0x00;
+    CLKCTRL.MCLKCTRLB = 0x00; // divider = 1 => 20MHz
+}
 
+int main(void) {
     // init
+    clk_init();
     dbg_init();
+
+    int8_t i = 0;
+    PORTC.DIRSET = PIN0_bm;
 
     // loop
     while (1) {
-        puts("hello world!\r\n");
-        _delay_ms(500);
+        printf("hello world! %d\r\n", i++);
+        PORTC.OUTTGL = PIN0_bm;
+        _delay_ms(100);
     }
 }
