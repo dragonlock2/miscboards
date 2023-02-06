7d47a44cbd14f4303ff4e934b0c0b879a0549775
Matthew Tran <matthewlamtran@berkeley.edu>
2023-02-05 23:22:59 -0800

    added FAT16/32 read-only implementation

--- avr/lightsaber/src/sdfat.py
+++ avr/lightsaber/src/sdfat.py
@@ -1,6 +1,8 @@
 import jabi
 import time
 import struct
+from pathlib import PosixPath
+from math import ceil
 
 class SD:
     BLOCK_LEN = 512
@@ -53,7 +55,7 @@ class SD:
             time.sleep(0.01)
         
         # bump up clock
-        self.dev.spi_set_freq(10000000, self.id)
+        self.dev.spi_set_freq(25000000, self.id)
         
         # CMD58 - read OCR
         self.CMD(58)
@@ -172,14 +174,171 @@ class SD:
                 raise Exception('no response')
         return [b] + self.dev.spi_transceive([0xFF]*(length-1), self.id)
 
+class FAT:
+    BYTES_PER_SECTOR = 512
+
+    def __init__(self, sd):
+        self.sd = sd
+
+        p1 = self.sd.read_block(0)[446:446+16]
+        if p1[4] not in [0x0B, 0x0C, 0xE]:
+            raise Exception(f'unknown filesystem type: {p1[4]}')
+        self.volume_id = struct.unpack('<I', bytes(p1[8:8+4]))[0]
+        boot_sect = self.sd.read_block(self.volume_id)
+
+        print(f'formatting OS: {"".join(chr(c) for c in boot_sect[3:3+8]).strip()}')
+        self.sectors_per_cluster = struct.unpack('<B', bytes(boot_sect[13:13+1]))[0]
+        self.rsvd_sectors = struct.unpack('<H', bytes(boot_sect[14:14+2]))[0]
+        self.num_fat = struct.unpack('<B', bytes(boot_sect[16:16+1]))[0]
+        self.sectors_per_fat = struct.unpack('<H', bytes(boot_sect[22:22+2]))[0]
+        if self.sectors_per_fat: # FAT16
+            self.is_fat32 = False
+
+            print(f'drive name: {"".join(chr(c) for c in boot_sect[43:43+11]).strip()}')
+            self.num_root_entries = struct.unpack('<H', bytes(boot_sect[17:17+2]))[0]
+            self.num_sectors = struct.unpack('<H', bytes(boot_sect[19:19+2]))[0]
+            if self.num_sectors == 0:
+                self.num_sectors = struct.unpack('<I', bytes(boot_sect[32:32+4]))[0]
+
+            # compute some stuff
+            self.fat_sector = self.volume_id + self.rsvd_sectors
+            self.root_dir_sector = self.fat_sector + self.num_fat * self.sectors_per_fat
+            self.num_root_sectors = ceil(self.num_root_entries * 32 / self.BYTES_PER_SECTOR)
+            self.data_sector = self.root_dir_sector + self.num_root_sectors - 2 * self.sectors_per_cluster
+        else: # FAT32
+            self.is_fat32 = True
+
+            print(f'drive name: {"".join(chr(c) for c in boot_sect[71:71+11]).strip()}')
+            self.rsvd_sectors = struct.unpack('<H', bytes(boot_sect[14:14+2]))[0]
+            self.sectors_per_fat = struct.unpack('<I', bytes(boot_sect[36:36+4]))[0]
+
+            self.fat_sector = self.volume_id + self.rsvd_sectors
+            self.data_sector = self.fat_sector + self.num_fat * self.sectors_per_fat - 2 * self.sectors_per_cluster
+
+    def ls(self, name):
+        def ls_helper(dirs, cluster):
+            files = []
+            found = False
+            e = self.read_file_from_cluster(cluster)
+            for d in [e[i:i+32] for i in range(0, len(e), 32)]:
+                dirent = self.parse_dirent(d)
+                if dirent['end']:
+                    break
+                elif dirent['volume_name'] or dirent['hidden'] or dirent['free']:
+                    continue
+                elif len(dirs) == 0: # ls of root dir
+                    files.append({
+                        'name': dirent['name'],
+                        'dir': dirent['dir'],
+                        'size': dirent['size'],
+                        'first_cluster': dirent['first_cluster']
+                    })
+                elif dirent['dir'] and dirs[0] == dirent['name']:
+                    cluster = dirent['first_cluster']
+                    found = True
+                    break
+            if len(dirs) == 0:
+                return files
+            elif found:
+                return ls_helper(dirs[1:], cluster)
+            else:
+                return None
+
+        start_cluster = 2 # root sector for FAT32
+        dirs = [d.upper() for d in PosixPath(name).parts[1:]]
+        if not self.is_fat32: # FAT16 has fixed root directory
+            files = []
+            end, found = False, False
+            for i in range(self.num_root_sectors): # quite a few, faster if one at a time
+                e = self.sd.read_block(self.root_dir_sector + i)
+                for d in [e[i:i+32] for i in range(0, len(e), 32)]:
+                    dirent = self.parse_dirent(d)
+                    if dirent['end']:
+                        end = True
+                        break
+                    elif dirent['volume_name'] or dirent['hidden'] or dirent['free']:
+                        continue
+                    elif len(dirs) == 0: # ls of root dir
+                        files.append({
+                            'name': dirent['name'],
+                            'dir': dirent['dir'],
+                            'size': dirent['size'],
+                            'first_cluster': dirent['first_cluster']
+                        })
+                    elif dirent['dir'] and dirs[0] == dirent['name']:
+                        start_cluster = dirent['first_cluster']
+                        end, found = True, True
+                if end:
+                    break
+            if len(dirs) == 0:
+                return files
+            elif not found:
+                return None
+            dirs = dirs[1:]
+
+        return ls_helper(dirs, start_cluster)
+
+    def read(self, name):
+        filename = PosixPath(name).parts[-1].upper()
+        dirs = str(PosixPath(name).parent)
+        dirents = self.ls(dirs)
+        if not dirents:
+            return None
+        for dirent in dirents:
+            if dirent['name'] == filename:
+                return self.read_file_from_cluster(dirent['first_cluster'])[:dirent['size']]
+        return None
+
+    # low-level helpers
+    def read_file_from_cluster(self, cluster):
+        data = self.read_cluster(cluster)
+        while cluster := self.next_cluster(cluster):
+            data.extend(self.read_cluster(cluster))
+        return bytes(data)
+
+    def read_cluster(self, cluster):
+        data = []
+        for d in self.sd.read_blocks(self.data_sector + cluster * self.sectors_per_cluster, self.sectors_per_cluster):
+            data.extend(d)
+        return data
+
+    def next_cluster(self, cluster):
+        bpe = 4 if self.is_fat32 else 2
+        eps = self.BYTES_PER_SECTOR // bpe
+        s = cluster // eps
+        i = (cluster % eps) * bpe
+        e = struct.unpack('<I' if self.is_fat32 else '<H', bytes(self.sd.read_block(self.fat_sector + s)[i:i+bpe]))[0]
+        if self.is_fat32:
+            # upper 4 bits reserved
+            e = e & 0x0FFFFFFF
+            return e if e < 0x0FFFFFF6 else None
+        else:
+            return e if e < 0xFFF7 else None
+
+    def parse_dirent(self, dirent):
+        ext = ''.join(chr(c) for c in dirent[8:11]).strip()
+        first_cluster = struct.unpack('<H', bytes(dirent[26:28]))[0]
+        if self.is_fat32:
+            first_cluster += struct.unpack('<H', bytes(dirent[20:22]))[0] << 16
+        return {
+            'end': dirent[0] == 0x00,
+            'volume_name': bool(dirent[11] & 0x08),
+            'dir': bool(dirent[11] & 0x10),
+            'hidden': bool(dirent[11] & 0x02),
+            'free': bool(dirent[0] == 0xe5),
+            'name': ''.join(chr(c) for c in dirent[0:8]).strip() + ('.' + ext if ext else ''),
+            'size': struct.unpack('<I', bytes(dirent[28:32]))[0],
+            'first_cluster': first_cluster,
+        }
+
 if __name__ == '__main__':
     # for k66f_breakout
     CS_PIN  = 7
     SPI_IDX = 1
 
     dev = jabi.USBInterface.list_devices()[0]
-    try:
-        sd = SD(dev, SPI_IDX, CS_PIN)
-    except:
-        print('failed once, trying again')
-        sd = SD(dev, SPI_IDX, CS_PIN)
+    sd = SD(dev, SPI_IDX, CS_PIN)
+    fat = FAT(sd)
+
+    print(fat.ls('/'))
+    print(fat.read('/a/b/c/cluster.md'))
