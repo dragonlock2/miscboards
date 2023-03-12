3522ea86f419d8b8428dd7661c8c70a015184fbc
Matthew Tran <matthewlamtran@berkeley.edu>
2023-03-12 04:24:00 -0700

    PIC blinky

---
+++ pic/solarFRAM/src/config.h
@@ -0,0 +1,14 @@
+#define _XTAL_FREQ 500000L
+
+#pragma config CLKOUTEN = OFF
+#pragma config BOREN    = OFF
+#pragma config CP       = OFF
+#pragma config PWRTE    = OFF
+#pragma config WDTE     = OFF
+#pragma config FOSC     = INTOSC
+
+#pragma config LVP     = ON
+#pragma config LPBOREN = OFF
+#pragma config STVREN  = OFF
+#pragma config PLLEN   = OFF
+#pragma config WRT     = OFF
--- pic/solarFRAM/src/main.c
+++ pic/solarFRAM/src/main.c
@@ -1,3 +1,14 @@
+#include <xc.h>
+#include "config.h"
+
 int main(void) {
-    return 0;
+    OSCCONbits.IRCF = 0b1010; // select FOSC = MFINTOSC
+
+    TRISA2 = 0;
+    while (1) {
+        RA2 = 1;
+        __delay_ms(500);
+        RA2 = 0;
+        __delay_ms(500);
+    }
 }
