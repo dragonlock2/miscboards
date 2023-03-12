5574653fa4d0b3518303a78419f39615d6e6091a
Matthew Tran <matthewlamtran@berkeley.edu>
2023-03-12 05:18:44 -0700

    flashing PIC from command line

--- pic/solarFRAM/src/main.c
+++ pic/solarFRAM/src/main.c
@@ -2,13 +2,13 @@
 #include "config.h"
 
 int main(void) {
-    OSCCONbits.IRCF = 0b1010; // select FOSC = MFINTOSC
+    OSCCONbits.IRCF = 0b1010; // select FOSC = 500kHz
 
     TRISA2 = 0;
     while (1) {
         RA2 = 1;
-        __delay_ms(500);
+        __delay_ms(250);
         RA2 = 0;
-        __delay_ms(500);
+        __delay_ms(250);
     }
 }
