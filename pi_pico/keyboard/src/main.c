6804a31426a65290a311ceb52b1e7701c15ed494
Matthew Tran <matthewlamtran@berkeley.edu>
2023-03-22 01:54:00 -0700

    added multicore thread

--- pi_pico/keyboard/src/main.c
+++ pi_pico/keyboard/src/main.c
@@ -1,5 +1,6 @@
 #include <pico/stdlib.h>
 #include <pico/cyw43_arch.h>
+#include <pico/multicore.h>
 #include "kscan.h"
 #include "ws2812b.h"
 
@@ -8,6 +9,25 @@ static bool ticker(repeating_timer_t *rt) {
     cpu0_tick = true;
     cpu1_tick = true;
     return true;
+}
+
+static void cpu0_thread(void) {
+    while (true) {
+        while (!cpu0_tick);
+        // TODO cpu0 - BLE, USB, kscan, encoder
+        kscan_run1ms();
+
+        cpu0_tick = false;
+    }
+}
+
+static void cpu1_thread(void) {
+    while (true) {
+        while (!cpu1_tick);
+        // TODO cpu1 - LEDs @ 1kHz, SSD1306 @ 60FPS, sleep req
+
+        cpu1_tick = false;
+    }
 }
 
 int main() {
@@ -19,26 +39,6 @@ int main() {
     repeating_timer_t timer;
     add_repeating_timer_ms(1, ticker, NULL, &timer);
 
-    bool prev[KSCAN_ROWS][KSCAN_COLS] = {0};
-    while (true) {
-        while (!cpu0_tick);
-        kscan_run1ms();
-
-        for (uint i = 0; i < KSCAN_ROWS; i++) {
-            for (uint j = 0; j < KSCAN_COLS; j++) {
-                bool c = kscan_read(i, j);
-                if (c && !prev[i][j]) {
-                    printf("%d, %d pressed\r\n", i, j);
-                } else if (!c && prev[i][j]) {
-                    printf("%d, %d released\r\n", i, j);
-                }
-                prev[i][j] = c;
-            }
-        }
-
-        cpu0_tick = false;
-    }
-
-    // TODO cpu0 - BLE, USB, kscan, encoder
-    // TODO cpu1 - LEDs @ 1kHz, SSD1306 @ 60FPS, sleep req
+    multicore_launch_core1(cpu1_thread);
+    cpu0_thread();
 }
