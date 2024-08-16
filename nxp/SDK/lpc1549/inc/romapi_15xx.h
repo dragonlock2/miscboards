4fb3c409d03c58f8687abee2036c82df9020681d
Matthew Tran <matthewlamtran@berkeley.edu>
2024-08-16 05:35:10 -0700

    randomize mac based on uid

--- nxp/SDK/lpc1549/inc/romapi_15xx.h
+++ nxp/SDK/lpc1549/inc/romapi_15xx.h
@@ -99,9 +99,9 @@ typedef struct {
 /**
  * @brief LPC15XX IAP_ENTRY API function type
  */
-static INLINE void iap_entry(unsigned int cmd_param[5], unsigned int status_result[4])
+static INLINE void iap_entry(uint32_t cmd_param[5], uint32_t status_result[4])
 {
-	((IAP_ENTRY_T) IAP_ENTRY_LOCATION)(cmd_param, status_result);
+	((IAP_ENTRY_T) IAP_ENTRY_LOCATION)((unsigned int*) cmd_param, (unsigned int*) status_result);
 }
 
 /**
--- nxp/lpc1549_oaspi/src/app.cpp
+++ nxp/lpc1549_oaspi/src/app.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <FreeRTOS.h>
 #include <task.h>
+#include <chip.h>
 #include "eth.h"
 #include "oaspi.h"
 #include "spi.h"
@@ -10,11 +11,19 @@
 uint8_t tud_network_mac_address[6] = {0x00, 0x50, 0xC2, 0x4B, 0x20, 0x00};
 
 extern "C" void app_main(void*) {
+    std::array<uint32_t, 4> uid;
+    Chip_IAP_ReadUID(&uid[0]);
+    uint8_t mac = 0xFF;
+    for (int i = 0; i < 16; i++) {
+        mac ^= reinterpret_cast<uint8_t*>(&uid[0])[i]; // randomize mac
+    }
+    tud_network_mac_address[5] = mac;
+
     usb_init();
     spi_init();
     oaspi_init();
     eth_init();
-    printf("booted!\r\n");
+    printf("booted! 0x%02x\r\n", mac);
 
     bool t = 0, r = 0, g = 0, b = 1;
     while (true) {
