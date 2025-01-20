fdd365b2be532eb92af3e270a4246b9cea5831a4
Matthew Tran <matthewlamtran@berkeley.edu>
2025-01-19 17:21:01 -0800

    add basic WebHID demo

--- nxp/lpc1549_oaspi/webhid/usb.js
+++ nxp/lpc1549_oaspi/webhid/usb.js
@@ -1,6 +1,9 @@
-let dev;
+const DEVICE_MAP = {
+    0xbc0189a1: "NCN26010",
+    0x0283bc91: "ADIN1100",
+};
 
-async function getDevice() {
+async function openDevice() {
     // get device
     dev = (await navigator.hid.requestDevice({filters: [
         { vendorId: 0xcafe, productId: 0x0069 },
@@ -59,16 +62,84 @@ async function getDevice() {
         if (resp.getUint8(0) != 0) { throw "Request rejected"; }
         return resp.getUint32(1, false);
     };
-
-    // TODO modify UI?
-    const setPLCA = async function (enable=true, leader=true, node_id=0, node_cnt=8) {
+    dev.setPLCA = async function (enable=true, leader=true, node_id=0, node_cnt=8) {
         const MMS = 4;
         await dev.regWrite(MMS, 0x8002, leader ? 0x0003 : 0x0002);
         await dev.regWrite(MMS, 0xCA01, enable << 15);
         await dev.regWrite(MMS, 0xCA02, (node_cnt << 8) | (node_id << 0));
     };
-    await setPLCA();
-    // await setPLCA(enable=false);
+
+    // modify UI
+    document.getElementById("device").onclick = closeDevice;
+    document.getElementById("device").innerHTML = "close";
+    document.getElementById("device").style.backgroundColor = "#80ff80";
+    document.getElementById("reg").style.display = "";
+    document.getElementById("plca").style.display = "";
+
+    let id = await dev.regRead(0, 0x0001);
+    let pn = DEVICE_MAP[id] || "unknown";
+    document.getElementById("reg-id").innerHTML = "id: 0x" + id.toString(16) + ` (${pn})`;
 }
 
-document.getElementById("open").addEventListener("click", getDevice);
+async function closeDevice() {
+    // close device
+    await dev.close();
+    await dev.forget();
+
+    // modify UI
+    document.getElementById("device").onclick = openDevice;
+    document.getElementById("device").innerHTML = "open";
+    document.getElementById("device").style.backgroundColor = "#ff8080";
+    document.getElementById("reg").style.display = "none";
+    document.getElementById("plca").style.display = "none";
+}
+
+async function regWrite() {
+    const mms = parseInt(document.getElementById("reg-write-mms").value);
+    const reg = parseInt(document.getElementById("reg-write-reg").value);
+    const val = parseInt(document.getElementById("reg-write-val").value);
+    try {
+        if (isNaN(mms) || isNaN(reg) || isNaN(val)) { throw new Error("invalid input!"); }
+        document.getElementById("reg-write-result").innerHTML = "waiting...";
+        await dev.regWrite(mms, reg, val);
+        document.getElementById("reg-write-result").innerHTML = "success!";
+    } catch {
+        document.getElementById("reg-write-result").innerHTML = "fail!";
+    }
+}
+
+async function regRead() {
+    const mms = parseInt(document.getElementById("reg-read-mms").value);
+    const reg = parseInt(document.getElementById("reg-read-reg").value);
+    try {
+        if (isNaN(mms) || isNaN(reg)) { throw new Error("invalid input!"); }
+        document.getElementById("reg-read-val").innerHTML = "waiting...";
+        document.getElementById("reg-read-val").innerHTML = "val: 0x" + (await dev.regRead(mms, reg)).toString(16);
+    } catch {
+        document.getElementById("reg-read-val").innerHTML = "fail!";
+    }
+}
+
+async function setPLCA() {
+    const enable   = document.getElementById("plca-enable").checked;
+    const leader   = document.getElementById("plca-leader").checked;
+    const node_id  = parseInt(document.getElementById("plca-node-id").value);
+    const node_cnt = parseInt(document.getElementById("plca-node-cnt").value);
+    try {
+        if (isNaN(node_id) || isNaN(node_cnt)) { throw new Error("invalid input!"); }
+        document.getElementById("plca-result").innerHTML = "waiting...";
+        await dev.setPLCA(enable, leader, node_id, node_cnt);
+        document.getElementById("plca-result").innerHTML = "success!";
+    } catch {
+        document.getElementById("plca-result").innerHTML = "fail!";
+    }
+}
+
+// setup UI
+document.getElementById("device").onclick = openDevice;
+document.getElementById("device").style.backgroundColor = "#ff8080";
+document.getElementById("reg").style.display = "none";
+document.getElementById("plca").style.display = "none";
+document.getElementById("reg-write-do").onclick = regWrite;
+document.getElementById("reg-read-do").onclick = regRead;
+document.getElementById("plca-do").onclick = setPLCA;
