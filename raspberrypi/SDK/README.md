e15be2ad7ace30b11e91445bb2f60001c5b21019
Matthew Tran <matthewlamtran@berkeley.edu>
2025-02-02 03:53:30 -0800

    add raspberry pi openocd

--- raspberrypi/SDK/README.md
+++ raspberrypi/SDK/README.md
@@ -1,3 +1,15 @@
 # SDK
 
-To use RISC-V, download the [latest toolchain](https://github.com/raspberrypi/pico-sdk-tools/releases/tag/v2.0.0-5) and place it under `SDK/riscv-toolchain`.
+## OpenOCD
+
+Run the following to build OpenOCD from source.
+```
+cd openocd
+./bootstrap
+./configure --disable-werror
+make
+```
+
+## RISC-V
+
+Download the [latest toolchain](https://github.com/raspberrypi/pico-sdk-tools/releases/tag/v2.0.0-5) and place it under `SDK/riscv-toolchain`.
--- raspberrypi/SDK/toolchain.cmake
+++ raspberrypi/SDK/toolchain.cmake
@@ -1,11 +1,14 @@
 set(PICO_SDK_PATH ${CMAKE_CURRENT_LIST_DIR}/pico-sdk)
 include(${PICO_SDK_PATH}/pico_sdk_init.cmake)
 
+set(OPENOCD_PATH ${CMAKE_CURRENT_LIST_DIR}/openocd)
+set(OPENOCD_BIN  ${OPENOCD_PATH}/src/openocd -s ${OPENOCD_PATH}/tcl -f interface/cmsis-dap.cfg)
+
 if(PICO_PLATFORM STREQUAL "rp2040")
     set(OPENOCD_TARGET rp2040.cfg)
 elseif(PICO_PLATFORM STREQUAL "rp2350-arm-s" OR PICO_PLATFORM STREQUAL "rp2350")
     set(OPENOCD_TARGET rp2350.cfg)
-    elseif(PICO_PLATFORM STREQUAL "rp2350-riscv")
+elseif(PICO_PLATFORM STREQUAL "rp2350-riscv")
     set(PICO_TOOLCHAIN_PATH ${CMAKE_CURRENT_LIST_DIR}/../SDK/riscv-toolchain)
     set(OPENOCD_TARGET rp2350-riscv.cfg)
 else()
@@ -45,11 +48,12 @@ function(pico_finalize)
 
     # boilerplate helpers
     add_custom_target(flash picotool load -f ${PROJECT_NAME}.uf2 DEPENDS ${PROJECT_NAME})
-    add_custom_target(flash_dbg openocd -f interface/cmsis-dap.cfg -f target/${OPENOCD_TARGET} -c "adapter speed 5000"
+    add_custom_target(flash_dbg ${OPENOCD_BIN} -f target/${OPENOCD_TARGET} -c "adapter speed 5000"
         -c "program ${PROJECT_NAME}.elf verify reset exit" DEPENDS ${PROJECT_NAME})
-    add_custom_target(server openocd -f interface/cmsis-dap.cfg -f target/${OPENOCD_TARGET} -c "adapter speed 5000" DEPENDS ${PROJECT_NAME})
-    add_custom_target(server_rtt openocd -f interface/cmsis-dap.cfg -f target/${OPENOCD_TARGET} -c "adapter speed 5000"
+    add_custom_target(server ${OPENOCD_BIN} -f target/${OPENOCD_TARGET} -c "adapter speed 5000" DEPENDS ${PROJECT_NAME})
+    add_custom_target(server_rtt ${OPENOCD_BIN} -f target/${OPENOCD_TARGET} -c "adapter speed 5000"
         -c "rtt setup 0x20000000 8192 \"SEGGER RTT\"" -c "init" -c "rtt start" -c "rtt server start 19021 0" DEPENDS ${PROJECT_NAME})
+    add_custom_target(gdb gdb ${PROJECT_NAME}.elf -ex "tar ext localhost:3333" -ex "set mem inaccessible-by-default on" DEPEND ${PROJECT_NAME})
 endfunction()
 
 set_source_files_properties( # disable warnings for some files
